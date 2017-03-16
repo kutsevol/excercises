36ab62d4010e24dc28121c06143f7f90ff47341a
Artur Kutsevol <arthur.kutsevol@gmail.com>
2017-03-16 14:14:19 +0200

    Add coding: utf-8. And change task description.

--- codility/odd_occurrences_ln_array.py
+++ codility/odd_occurrences_ln_array.py
@@ -1,4 +1,5 @@
-# Task DESCRIPTION
+# coding: utf-8
+# Task description
 
 """
 A non-empty zero-indexed array A consisting of N integers is given. The array
@@ -8,9 +9,9 @@ left unpaired.
 
 For example, in array A such that:
 
-  A[0] = 9  A[1] = 3  A[2] = 9
-  A[3] = 3  A[4] = 9  A[5] = 7
-  A[6] = 9
+A[0] = 9  A[1] = 3  A[2] = 9
+A[3] = 3  A[4] = 9  A[5] = 7
+A[6] = 9
 the elements at indexes 0 and 2 have value 9,
 the elements at indexes 1 and 3 have value 3,
 the elements at indexes 4 and 6 have value 9,
@@ -24,9 +25,9 @@ returns the value of the unpaired element.
 
 For example, given array A such that:
 
-  A[0] = 9  A[1] = 3  A[2] = 9
-  A[3] = 3  A[4] = 9  A[5] = 7
-  A[6] = 9
+A[0] = 9  A[1] = 3  A[2] = 9
+A[3] = 3  A[4] = 9  A[5] = 7
+A[6] = 9
 the function should return 7, as explained in the example above.
 
 Assume that:
